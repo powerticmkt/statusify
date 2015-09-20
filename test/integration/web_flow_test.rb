8a5004670edd533d18d59527a037ead22315ab96
Umang Raghuvanshi <umang.raghuvanshi@gmail.com>
2015-09-20 14:13:10 +0530

    Improve WebFlow integration test.
    Refractor sign in test to use sign_in method instead of posting data manually.
    Add test for issue creation.

--- test/integration/web_flow_test.rb
+++ test/integration/web_flow_test.rb
@@ -18,8 +18,37 @@ class WebFlowTest < ActionDispatch::IntegrationTest
   test 'sign in' do
     get '/sign_in'
     assert_response :success, 'Failed to get sign in page'
-    post_via_redirect '/session', 'session[email]' => User.first.email, 'session[password]' => 'password'
-    assert_response :success
+    # Checking with valid credentials
+    sign_in(User.first.email, 'password')
+    assert_response :success, 'Failed to log in in via valid credentials.'
+    # Sign out
+    delete '/sign_out'
+    # Checking with invalid credentials
+    sign_in(User.first.email, 'foobar')
+    assert_response :unauthorized, 'Invalid credentials work.'
+  end
+
+  test 'create incident' do
+    sign_in(User.first.email, 'password')
+    get '/incidents/new'
+    assert_response :success, 'Failed to get new incident page'
+    # Create an incident same as the first one, provided by the fixtures
+    r = create_incident(Incident.first)
+    assert_equal 'success', r.headers['status'], 'Could not create valid incident'
+    # Create an invalid incident
+    r = create_incident(Incident.new(name: nil, message: nil, component: nil))
+    assert_equal 'failed', r.headers['status'], 'Could create invalid incident'
+  end
+
+  def sign_in(email, password)
+    post_via_redirect '/session', 'session[email]' => email, 'session[password]' => password
+    return response
+  end
+
+  def create_incident(i)
+    return if i.class != Incident
+    post '/incidents', 'incident[name]': i.name, 'incident[message]': i.message, 'incident[component]': i.component
+    return response
   end
 
 end
