07b1b2cbaabc62fd0851dec09f3f1e832f174f59
Umang Raghuvanshi <umang.raghuvanshi@gmail.com>
2015-09-22 14:09:49 +0530

    Silence ActiveRecord SQL logs in production environments.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Silence ActiveRecord SQL logs.
+  ActiveRecord::Base.logger.level = 1
 end
