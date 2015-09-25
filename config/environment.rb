922ed5f064b709a18b8d3e27480cfad64643637f
Umang Raghuvanshi <umang.raghuvanshi@gmail.com>
2015-09-25 20:36:22 +0530

    Remove reduant initializer and reload APP_CONFIG every boot.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Loads global application config from config/config.yml
+APP_CONFIG = YAML.load(ERB.new(File.read("#{Rails.root}/config/config.yml")).result)[Rails.env]
--- config/initializers/load_config.rb
+++
@@ -1,2 +0,0 @@
-# Loads global application config from config/config.yml
-APP_CONFIG = YAML.load(ERB.new(File.read("#{Rails.root}/config/config.yml")).result)[Rails.env]
