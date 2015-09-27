4993e6a2e760bf205651cd7e7470808308bf15a1
Umang Raghuvanshi <umang.raghuvanshi@gmail.com>
2015-09-27 19:27:22 +0530

    Adds severity to incidents.
    This adds the severity value to incidents and colours them accordingly.

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -10,8 +10,20 @@ class Incident < ActiveRecord::Base
   validates_presence_of :component
   validates_presence_of :public
   validates_presence_of :user_id
+  validates_presence_of :severity
   # We have to hook AR to make our Incidents/Events thing work. Tricky.
   accepts_nested_attributes_for :events
+
+  def convert_to_level
+    # Converts incident severity to bootstrap color, for use with text-<level>
+    # Like text-danger, text-warning
+    case self.severity
+      when 'major'
+        'danger'
+      when 'minor'
+        'warning'
+    end
+  end
 
   private
 
