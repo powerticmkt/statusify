9a938ef48aabec91802d5e90e8b50bd0d8d69d3c
Umang Raghuvanshi <umang.raghuvanshi@gmail.com>
2015-09-29 17:45:47 +0530

    Add tests for ApplicationHelper.
    This adds the tests for ApplicationHelper.

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  # Even the application helper does a lot of work. We gotta test it,
+  # cuz even it might make things blow up big-time.
+  include ApplicationHelper
+
+  def signed_in?
+    # Just a senseless stub that makes the visible incidents test work.
+    false
+  end
+
+  test 'check dated_incidents' do
+    @dated_incidents = dated_incidents
+    @dated_incidents.each do |date, i|
+      # We got to have some incidents in our fixture.
+      # If this fails, everything else will fail in due time.
+      assert_equal Incident::ActiveRecord_Relation, i.class, 'dated_incidents is not returning incidents'
+    end
+  end
+
+  test 'check all_incidents' do
+    @incidents = all_incidents
+    assert_equal Incident::ActiveRecord_Relation, @incidents.class, 'all_incidents is not returning incidents.'
+  end
+
+  test 'check visible_incidents' do
+    @current_user = nil
+    @visible_incidents = visible_incidents
+    assert_equal Incident, @visible_incidents.first.class, 'visible_incidents is not returning incidents.'
+    assert_equal true, @visible_incidents.first.public?, 'Shpwing private incidents.'
+  end
+
+end
