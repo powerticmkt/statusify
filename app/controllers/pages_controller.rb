8851a6df3edb249a8be8b9120ec13ebef0f9d947
Umang Raghuvanshi <umang.raghuvanshi@gmail.com>
2015-09-30 17:51:37 +0530

    Fix typo.
    [ci skip]

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,6 @@ class PagesController < ApplicationController
   end
   def history
     # The incident history. Shows all the ancient incidents since the index page shows only five of them.
-    @dated_incidents |e|= dated_incidents
+    @dated_incidents ||= dated_incidents
   end
 end
