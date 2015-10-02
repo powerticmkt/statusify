b53c27657f63fb7e038f1aefda64e09ccd86fb5d
Umang Raghuvanshi <umang.raghuvanshi@gmail.com>
2015-10-02 12:03:36 +0530

    Improve index page's performance.
    This improves index page's performance by caching the entire page for non signed-in users.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   def index
-    @incidents ||= Incident.all
   end
   def history
     # The incident history. Shows all the ancient incidents since the index page shows only five of them.
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,7 @@
 module PagesHelper
+  def cache_key_for_index
+    count = Incident.count
+    max_updated_at = Incident.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "incidents/all-#{count}-#{max_updated_at}"
+  end
 end
