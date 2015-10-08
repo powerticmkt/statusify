f41aeb48587e0b3f63befa246ae3a873cf292be2
Umang Raghuvanshi <umang.raghuvanshi@gmail.com>
2015-10-08 20:28:19 +0530

    Use HTML titles.
    This uses HTML titles and defaults to the app name if none is provided.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,12 @@ module ApplicationHelper
         nil
     end
   end
+  def full_title
+    app_name ||= APP_CONFIG['name']
+    if :title
+      "#{content_for(:title)} | #{app_name}"
+    else
+      "#{app_name}"
+    end
+  end
 end
