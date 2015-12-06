77cafd0a05694b0f9721fab6f16fa4869f8614c4
Andrew Scott <amscott@openmailbox.org>
2015-12-06 00:48:23 +0000

    Convert specs to RSpec 3.3.2 syntax with Transpec
    This conversion is done by Transpec 3.1.1 with the following command:
        transpec
    
    * 29 conversions
        from: it { should ... }
          to: it { is_expected.to ... }
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/controllers/regression/incidents_controller_spec.rb
+++ spec/controllers/regression/incidents_controller_spec.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 
 RSpec.describe IncidentsController, regressor: true do
   # === Routes (REST) ===
-  it { should route(:get, '/incidents/new').to('incidents#new', {}) }
-  it { should route(:post, '/incidents').to('incidents#create', {}) } 
-  it { should route(:get, '/incidents/1/edit').to('incidents#edit', {:id=>"1"}) }
-  it { should route(:patch, '/incidents/1/edit').to('incidents#update', {:id=>"1"}) } 
-  it { should route(:get, '/incidents/1/delete').to('incidents#delete', {:id=>"1"}) }
-  it { should route(:get, '/incidents/1/deactivate').to('incidents#deactivate', {:id=>"1"}) }
-  it { should route(:get, '/incidents/1').to('incidents#view', {:id=>"1"}) }
+  it { is_expected.to route(:get, '/incidents/new').to('incidents#new', {}) }
+  it { is_expected.to route(:post, '/incidents').to('incidents#create', {}) } 
+  it { is_expected.to route(:get, '/incidents/1/edit').to('incidents#edit', {:id=>"1"}) }
+  it { is_expected.to route(:patch, '/incidents/1/edit').to('incidents#update', {:id=>"1"}) } 
+  it { is_expected.to route(:get, '/incidents/1/delete').to('incidents#delete', {:id=>"1"}) }
+  it { is_expected.to route(:get, '/incidents/1/deactivate').to('incidents#deactivate', {:id=>"1"}) }
+  it { is_expected.to route(:get, '/incidents/1').to('incidents#view', {:id=>"1"}) }
   # === Callbacks (Before) ===
-  it { should use_before_filter(:verify_authenticity_token) }
-  it { should use_before_filter(:set_xhr_redirected_to) }
-  it { should use_before_filter(:set_request_method_cookie) }
-  it { should use_before_filter(:require_login) }
+  it { is_expected.to use_before_filter(:verify_authenticity_token) }
+  it { is_expected.to use_before_filter(:set_xhr_redirected_to) }
+  it { is_expected.to use_before_filter(:set_request_method_cookie) }
+  it { is_expected.to use_before_filter(:require_login) }
   # === Callbacks (After) ===
-  it { should use_after_filter(:abort_xdomain_redirect) }
-  it { should use_after_filter(:verify_same_origin_request) }
+  it { is_expected.to use_after_filter(:abort_xdomain_redirect) }
+  it { is_expected.to use_after_filter(:verify_same_origin_request) }
   # === Callbacks (Around) ===
   
 end
--- spec/controllers/regression/subscribers_controller_spec.rb
+++ spec/controllers/regression/subscribers_controller_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 RSpec.describe SubscribersController, regressor: true do
   # === Routes (REST) ===
-  it { should route(:post, '/subscribers/new').to('subscribers#create', {}) } 
-  it { should route(:get, '/subscribers/activate/1').to('subscribers#activate', {:activation_key=>"1"}) }
-  it { should route(:get, '/subscribers/unsubscribe/1').to('subscribers#unsubscribe', {:activation_key=>"1"}) }
+  it { is_expected.to route(:post, '/subscribers/new').to('subscribers#create', {}) } 
+  it { is_expected.to route(:get, '/subscribers/activate/1').to('subscribers#activate', {:activation_key=>"1"}) }
+  it { is_expected.to route(:get, '/subscribers/unsubscribe/1').to('subscribers#unsubscribe', {:activation_key=>"1"}) }
   # === Callbacks (Before) ===
-  it { should use_before_filter(:verify_authenticity_token) }
-  it { should use_before_filter(:set_xhr_redirected_to) }
-  it { should use_before_filter(:set_request_method_cookie) }
+  it { is_expected.to use_before_filter(:verify_authenticity_token) }
+  it { is_expected.to use_before_filter(:set_xhr_redirected_to) }
+  it { is_expected.to use_before_filter(:set_request_method_cookie) }
   # === Callbacks (After) ===
-  it { should use_after_filter(:abort_xdomain_redirect) }
-  it { should use_after_filter(:verify_same_origin_request) }
+  it { is_expected.to use_after_filter(:abort_xdomain_redirect) }
+  it { is_expected.to use_after_filter(:verify_same_origin_request) }
   # === Callbacks (Around) ===
   
 end
